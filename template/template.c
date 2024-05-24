63b2892cdea70e4a0cfc1ee3de204526ff8eae2f
Manula <md920@cam.ac.uk>
2024-05-24 10:53:56 +0100

    arithmetic test in template

--- template/template.c
+++ template/template.c
@@ -1,5 +1,24 @@
-int
-main(void)
+int main(void)
 {
+	volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
+	int A = 3;
+	int B = 5;
+	int counter = A + B;
+	while(counter>0)
+	{
+		for (int i = 0; i < 100000; i++)
+		{
+			*gDebugLedsMemoryMappedRegister = 0xFF;
+		}
+		for (int i = 0; i < 100000; i++)
+		{
+			*gDebugLedsMemoryMappedRegister = 0x00;
+		}
+		counter -= 1;
+	}
+	for (int i = 0; i < 5000000; i++)
+	{
+		*gDebugLedsMemoryMappedRegister = 0x00;
+	}
 	return 0;
 }
