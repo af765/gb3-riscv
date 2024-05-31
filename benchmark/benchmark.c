190300fb6026b2694f8cb30117cee884332046af
Manula <md920@cam.ac.uk>
2024-05-31 10:58:24 +0100

    new benchmark

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -1,37 +1,99 @@
-void ALU_test(int A)
+void Instruction_test(int A)
 {
 	int B = 8979;
 	int C = 1;
+	unsigned int D = 17;
+	
+	unsigned char ch = 'A'; //SB
+	unsigned short sh = 0x1234; //SH
+	unsigned char *cptr = &ch;
+	unsigned short *sptr = &sh;
+	signed char ch2 = 'q';
+	signed short sh2 = 0x5678;
+	signed char *cptr2 = &ch2;
+	signed short *sptr2 = &sh2;
 
-	A += 2796; //Add
-	A += C; //Add
-	A -= B; //Subtract
+	ch2 = *cptr2; //LB
+	sh2 = *sptr2; //LH
 
-	A = A >> 2; //Shift
+	ch = *cptr; //LBU
+	sh = *sptr; //LHU
 
-	A = A ^ 5894; //XOR
-	A = A | B; //Bitwise OR
-	A = A & B; //Bitwise AND
+	A += 2796; //ADDI
+	A += C; //ADD
+	A -= B; //SUB
+
+	if (A % 2 == 0 && D < 20) //branching
+	{
+		A = A >> 2; //SRAI
+		A = A >>C; //SRA
+		D = D >> 2; //SRLI
+		D = D >> C; //SRL
+
+		D = D << 2; //SLLI
+		D = D << C; //SLL
+
+		int E = (A < 100) ? 1:0; //SLTI
+		E = A < B; //SLT
+		unsigned int F = (D < 59) ? 1:0; //SLTUI
+		F = A < D; //SLTU
+
+		A = A ^ B; //XOR
+		A = A ^ 10; //XORI
+		A = A | B; //OR
+		A = A | 15; //ORI
+		A = A & B; //AND
+	}
+}
+
+//This should visably blink the LED twice to check the ALU is working
+void check_func(void){
+	volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
+	int cond = 4 + 3 - 5;
+	for(int i=0; i<cond; i++){
+		*gDebugLedsMemoryMappedRegister = 0xFF;
+
+		for (int j = 0; j < 400000; j++)
+			;
+
+		*gDebugLedsMemoryMappedRegister = 0x00;
+
+		for (int j = 0; j < 400000; j++)
+			;
+	}
 }
 
 int main(void)
 {
+	//Verify the ALU works
+	check_func();
+
 	volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
 	int A = 1;
 	int counter = 0;
-	for (int i = 0; i < 100000; i++)
-	{
-		*gDebugLedsMemoryMappedRegister = 0xFF;
+	
+	//Flash to indicate start
+	*gDebugLedsMemoryMappedRegister = 0xFF; 
+	for (int j = 0; j < 200000; j++)
+		;
+	*gDebugLedsMemoryMappedRegister = 0x00;
+
+	//Stress the processor
+	while(counter<300000){
+		Instruction_test(A+counter);
+		counter ++;
 	}
-	while(counter<500000)
-	{
-		*gDebugLedsMemoryMappedRegister = 0x00;
-		ALU_test(A+counter);
-		counter += 1;
-	}
-	for (int i = 0; i < 100000; i++)
-	{
-		*gDebugLedsMemoryMappedRegister = 0xFF;
-	}
+	
+	for (int j = 0; j < 200000; j++)
+		;
+
+	//Flash to indicate end
+	*gDebugLedsMemoryMappedRegister = 0xFF;
+	for (int j = 0; j < 200000; j++)
+		;
+	*gDebugLedsMemoryMappedRegister = 0x00;
+
+	for (int j = 0; j < 400000; j++)//spin at finish - avoid looping causing issues
+		;
 	return 0;
 }
